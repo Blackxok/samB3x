811db4ece207f64731986c47e5a74ee8ea24fcf4
Blackxok <nasridoni1@gmail.com>
2024-12-26 13:29:05 +0500

    style starts

--- frontend/src/layout/layout.tsx
+++ frontend/src/layout/layout.tsx
@@ -1,19 +1,20 @@
 import { FunctionComponent } from 'react'
 import Footer from './footer/footer'
 import Header from './header/header'
+import styles from './layout.module.css'
 import { LayoutProps } from './layout.props'
 import Sidebar from './sidebar/sidebar'
 
 function Layout({ children }: LayoutProps): JSX.Element {
 	return (
-		<>
+		<div className={styles.wrapper}>
 			<Header />
 			<div>
 				<Sidebar />
 				<div>{children}</div>
 			</div>
 			<Footer />
-		</>
+		</div>
 	)
 }
 export const withLayout = <T extends Record<string, unknown>>(Component: FunctionComponent<T>) => {
