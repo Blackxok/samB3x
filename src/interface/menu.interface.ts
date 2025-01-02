366a5d6357ce482120350bbfa235bb9069c4830b
Blackxok <nasridoni1@gmail.com>
2025-01-02 15:47:28 +0500

    +some menu sidebar

--- src/interface/menu.interface.ts
+++ src/interface/menu.interface.ts
@@ -4,6 +4,7 @@ export interface MenuItem {
 	_id: {
 		secondCategory: string
 	}
+	isOpen?: boolean
 	pages: PageItem[]
 }
 export interface PageItem {
--- src/layout/menu/menu.tsx
+++ src/layout/menu/menu.tsx
@@ -1,5 +1,5 @@
 import { AppContext } from '@/src/context/app.context'
-import { IfirstLevelMenu } from '@/src/interface/menu.interface'
+import { IfirstLevelMenu, PageItem } from '@/src/interface/menu.interface'
 import { PageCategory } from '@/src/interface/page.interface'
 import cn from 'classnames'
 import Link from 'next/link'
@@ -19,20 +19,55 @@ export default function Menu(): JSX.Element {
 			<>
 				{firstLevelMenu.map(c => (
 					<div key={c.route}>
-						<Link href={`/${c.route}`}>
-							<div
-								className={cn(styles.firstLevel, {
-									[styles.firstLevelActive]: c.id === firstCategory,
-								})}
-							>
-								{c.icon}
-								<span>{c.name}</span>
-							</div>
-						</Link>
+						<>
+							<Link href={`/${c.route}`}>
+								<div
+									className={cn(styles.firstLevel, {
+										[styles.firstLevelActive]: c.id === firstCategory,
+									})}
+								>
+									{c.icon}
+									<span>{c.name}</span>
+								</div>
+							</Link>
+							{c.id === firstCategory && buildSecondLevel(c)}
+						</>
 					</div>
 				))}
 			</>
 		)
+	}
+	const buildSecondLevel = (menuItem: IfirstLevelMenu) => {
+		return (
+			<div className={styles.secondBlock}>
+				{menu.map(q => (
+					<div key={q._id.secondCategory}>
+						<div className={styles.secondLevel}>{q._id.secondCategory}</div>
+						<div
+							className={cn(styles.secondLevelBlock, {
+								[styles.secondLevelBlockActive]: q.isOpen,
+							})}
+						>
+							{buildThirdLevel(q.pages, menuItem.route)}
+						</div>
+					</div>
+				))}
+			</div>
+		)
+	}
+
+	const buildThirdLevel = (pages: PageItem[], route: string) => {
+		return pages.map(w => (
+			<Link
+				key={w._id}
+				href={`/${route}/${w.alias}`}
+				className={cn(styles.thirdLevel, {
+					[styles.thirdLevelActive]: true,
+				})}
+			>
+				{w.title}17.42
+			</Link>
+		))
 	}
 
 	return <div className={styles.menu}>{buildFirstLevel()}</div>
