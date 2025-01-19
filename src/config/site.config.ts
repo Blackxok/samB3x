c8b6028c223065908102e83a20646359a995cdfc
Blackxok <nasridoni1@gmail.com>
2025-01-19 14:45:03 +0500

    ogImage

--- src/config/site.config.ts
+++ src/config/site.config.ts
@@ -6,7 +6,8 @@ export const siteConfig = {
 		author: 'Doniyor Nasriddiov',
 		keyword:
 			"noniyor nasriddiov, codeschool, dasturlash kurslari, dasturlashga oid darslar, reactjs uzbek tilida, vuejs uzbek tilida, redux uzbek tilida, codeschool, codeschool academy, bepul dasturlash, rezyume yozish, portfolio, codeschool javascript, codeschool raqamli avlod, javascript, reactjs, vuejs, javascript darslari, reactjs darslari, vuejs darslari, dasturlash darslari, o'zbek tilida dasturlash, reactjs o'zbek tilida, reactjs darslari o'zbek tilida, javascript darslari, javascript darslari o'zbek tilida, dasturash darslari o'zbek tilida, dasturlashni o'rganish, dasturlash, IT loyihalar o'zbek tilida",
-		ogImage: '../../public/logoPng.png',
+		ogImage:
+			'https://www.google.com/imgres?q=car&imgurl=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Fa%2Fa4%2F2019_Toyota_Corolla_Icon_Tech_VVT-i_Hybrid_1.8.jpg&imgrefurl=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FCar&docid=mbNbG1lcjhkU7M&tbnid=90RNgyKUjW4UPM&vet=12ahUKEwiCzK7qv4GLAxX-VvEDHfH6BmYQM3oECH8QAA..i&w=4925&h=2592&hcb=2&ved=2ahUKEwiCzK7qv4GLAxX-VvEDHfH6BmYQM3oECH8QAA',
 	},
 	baseURL: 'https://samb3x.vercel.app/',
 	logo: '../../public/logoPng.png',
